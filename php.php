24e0935fe7a3a7496de91a5f56b4c22d053a3c94
Printer <88195101+imprimeriberac@users.noreply.github.com>
2024-02-23 10:57:05 +0100

    Add files via upload

---
+++ php.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "public/view/template/header.php";
+require_once "public/view/content/acceuil.php";
+require_once "public/view/template/footer.php";
---
+++ public/view/template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr
+">
+<head>
+    <meta charset="UTF-8">
+   <link rel="stylesheet" href="public/css/styles .css">
+   
+   <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css">
+   <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-solid.css">
+   <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-regular.css">
+   <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-light.css">
+
+   <!-- Icon Font Stylesheet -->
+   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.8.2/css/all.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
